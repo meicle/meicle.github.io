2e145dfb032d548dc766eab6160a1c9a5a6011d6
meicle <meiclehaiara@outlook.com>
2018-11-23 03:30:37 -0200

    Side Menu

---
+++ loadpages.js
@@ -0,0 +1,14 @@
+$(function(){
+            $('#content').load('aboutme.html');
+
+            $( "#aboutme" ).click(function() {
+              $('#content').load('aboutme.html');
+            });
+            $( "#skills" ).click(function() {
+              $('#content').load('skills.html');
+            });
+            $( "#experience" ).click(function() {
+              $('#content').load('experience.html');
+            });
+            }
+);

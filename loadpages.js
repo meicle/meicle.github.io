6446a342324e5d268f56432b4f327097567f175e
meicle <meiclehaiara@outlook.com>
2018-12-01 01:28:07 -0200

    Menu added

--- loadpages.js
+++ loadpages.js
@@ -1,14 +1,22 @@
 $(function(){
-            $('#content').load('aboutme.html');
-
-            $( "#aboutme" ).click(function() {
-              $('#content').load('aboutme.html');
-            });
-            $( "#skills" ).click(function() {
-              $('#content').load('skills.html');
-            });
-            $( "#experience" ).click(function() {
-              $('#content').load('experience.html');
-            });
-            }
+  var sidebarlist = $(".list");
+  
+  $('#content').load('aboutme.html');
+  
+  $( "#aboutme" ).click(function() {
+    $('#content').load('aboutme.html');
+  });
+  $( "#skills" ).click(function() {
+    $('#content').load('skills.html');
+  });
+  $( "#experience" ).click(function() {
+    $('#content').load('experience.html');
+  });
+  $( ".item" ).click(function() {
+    sidebarlist.hide();
+  });
+  $( ".menubutton" ).click(function() {
+    sidebarlist.toggle();
+  });
+}
 );
